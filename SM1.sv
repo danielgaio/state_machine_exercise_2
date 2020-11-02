2e672fb2ea5da73db9758956851276dcccf199a9
Daniel <danielelielgaio@gmail.com>
2020-11-02 19:53:15 -0300

    Adição de comentários

--- SM1.sv
+++ SM1.sv
@@ -21,51 +21,64 @@
 `timescale 1ns/1ns
 
 module SM1 (
+
+    // o modulo possui três sinais de entrada e um de saida
     input reset, input clock, input entrada,
     output reg saida);
 
+
+    // criação de duas estruturas do tipo enum para guardar o estado atual e o próximo estado
     enum int unsigned { s0=0, s1=1, s2=2, s3=3, s4=4 } fstate, reg_fstate;
 
-    always_ff @(posedge clock)
-    begin
+
+    // processo que vai atualizar o estado atual a cada subida do clock
+    always_ff @(posedge clock) begin
+
         if (clock) begin
             //$display("fstate: %d", fstate);
             //$display("reg_fstate: %d", reg_fstate);
-	    $display("tempo atualizacao estado: %d", $time);
+	        $display("tempo atualizacao estado: %d", $time);
             fstate <= reg_fstate;
         end
+
     end
 
+
+    // processo que analisa as novas entradas e realiza a troca de estados e emissão do respectivo sinal de saida
     always_comb begin
+
         if (reset) begin
             reg_fstate <= s0;
             saida <= 1'b0;
         end
+
         else begin
-            //saida <= 1'b0;
+            
+
             case (fstate)
+
+
+
                 s0: begin
+
                     if (entrada) begin
                         reg_fstate <= s1;
-			saida <= 1'b1;
+			            saida <= 1'b1;
                     end else if (~(entrada)) begin
                         reg_fstate <= s0;
-			saida <= 1'b0;
+			            saida <= 1'b0;
                     // Inserting 'else' block to prevent latch inference
                     end else begin
                         reg_fstate <= s0;
-			saida <= 1'b0;
-		    end
+			            saida <= 1'b0;
+		            end
 
-                    //if (~(entrada))
-                        //saida <= 1'b0;
-                    //else if (entrada)
-                        //saida <= 1'b1;
-                    // Inserting 'else' block to prevent latch inference
-                    //else
-                        //saida <= 1'b0;
                 end
+
+
+
                 s1: begin
+
                     if (~(entrada))
                         reg_fstate <= s2;
                     else if (entrada)
@@ -73,6 +86,7 @@ module SM1 (
                     // Inserting 'else' block to prevent latch inference
                     else
                         reg_fstate <= s1;
+
 
                     if (~(entrada))
                         saida <= 1'b1;
@@ -82,7 +96,12 @@ module SM1 (
                     else
                         saida <= 1'b0;
                 end
+
+
+
+
                 s2: begin
+
                     if (~(entrada))
                         reg_fstate <= s4;
                     else if (entrada)
@@ -90,6 +109,7 @@ module SM1 (
                     // Inserting 'else' block to prevent latch inference
                     else
                         reg_fstate <= s2;
+
 
                     if (entrada)
                         saida <= 1'b0;
@@ -99,6 +119,9 @@ module SM1 (
                     else
                         saida <= 1'b0;
                 end
+
+
+
                 s3: begin
                     if (~(entrada))
                         reg_fstate <= s2;
@@ -108,6 +131,7 @@ module SM1 (
                     else
                         reg_fstate <= s3;
 
+
                     if (entrada)
                         saida <= 1'b1;
                     else if (~(entrada))
@@ -116,6 +140,10 @@ module SM1 (
                     else
                         saida <= 1'b0;
                 end
+
+
+
+
                 s4: begin
                     if (~(entrada))
                         reg_fstate <= s0;
@@ -125,6 +153,7 @@ module SM1 (
                     else
                         reg_fstate <= s4;
 
+
                     if (~(entrada))
                         saida <= 1'b0;
                     else if (entrada)
@@ -133,10 +162,15 @@ module SM1 (
                     else
                         saida <= 1'b0;
                 end
+
+
+
                 default: begin
                     saida <= 1'bx;
                     $display ("Reach undefined state");
                 end
+
+                
             endcase
         end
     end
--- state_machine_tb.sv
+++ state_machine_tb.sv
@@ -1,15 +1,18 @@
-// Code your testbench here
-// or browse Examples
+
 
 `timescale 1ns/1ns
 
 module state_machine_tb;
 
+
   // inputs of dut as reg
   reg clock_tb, reset_tb, entrada_tb;
 
+
   // outputs of dut as wire
   wire saida_tb;
+
+
 
   // Instanciando o módulo
   SM1 my_SM1_dut (
@@ -19,25 +22,36 @@ module state_machine_tb;
     .saida(saida_tb)
   );
 
+
+
   // etapa de reset
   initial begin
     reset_tb = 1;
   end
 
-  // Gerador de clock
 
-initial clock_tb = 0;
-always begin #5 clock_tb = ~clock_tb; $display("time: %d", $time); end
+
+  // Gerador de clock
+  initial clock_tb = 0;
+  always begin #5 clock_tb = ~clock_tb; $display("time: %d", $time); end
+
+
 
 
   initial begin
     
+
+    // desligar o reset após 10 unidades de tempo
     #10
     reset_tb = 0;
 
    
-    //$display("-----------");
-
+  
+    // inserção de alguns sinais no pino de entrada da maquina de estados
+    // a correspondência entre entrada estado e saidas geradas deve ser verificada na forma de onda
+    // a forma de onda a ser observada deve corresponder aos sinais internos do módulo da máquina de estados
+    // pois pelos sinais do testbench não é possivel visualisar os estados
+    
     entrada_tb = 1;
     #20
     entrada_tb = 0;
@@ -46,13 +60,6 @@ always begin #5 clock_tb = ~clock_tb; $display("time: %d", $time); end
 
 
 
-
-
-
-    //if (saida_tb == 0 && estado_atual_tb == 0) $display("saida ok"); else $display("saida not ok");
-    //$display("\t clock_tb, \t reset_tb, \t entrada_tb, \t saida_tb \t estado atual");
-    //$monitor("\t %b, \t\t %b, \t %b, \t\t %b \t\t%d", clock_tb, reset_tb, entrada_tb, saida_tb, estado_atual_tb);
-    //$display("-----------");
     $stop;
 
   end 
